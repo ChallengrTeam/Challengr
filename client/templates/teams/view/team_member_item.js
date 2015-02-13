e32f092fd8c97adbf7770ebb92ebbad0b3fe11ac
Jason Thai <jtxdev@gmail.com>
2015-02-14 01:37:20 +1100

    Added ability to remove members from a team

--- client/templates/teams/view/team_member_item.js
+++ client/templates/teams/view/team_member_item.js
@@ -3,3 +3,18 @@ Template.teamMemberItem.helpers({
   	return Meteor.users.findOne( { _id: this.memberId } );
   }
 });
+
+Template.teamMemberItem.events({
+  'click .remove': function(e) {
+    e.preventDefault();
+
+    Meteor.call('teamRemove', this.teamId, this.memberId, function(error, result) {
+      // display the error to the user and abort
+    if (error)
+      return alert(error.reason);
+
+    // Todo: check if member exists just incase
+      
+    });
+  }
+});
