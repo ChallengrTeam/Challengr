e8fd249e785b11b024b311164bc3fba935b8b83b
Jackie H <jackiehuynh93@gmail.com>
2015-02-15 15:39:36 +1100

    Changed fixtures to add clearer stuff

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,124 +1,52 @@
+var usernames = ['Anne', 'Bob', 'Catherine', 'Daniel'];
+
 if ( Meteor.users.find().count() === 0 ) {
-    Accounts.createUser({
-        username: 'tester1',
-        password: 'tester1'
-    });
-    Accounts.createUser({
-        username: 'tester2',
-        password: 'tester2'
+    usernames.forEach(function (entry) {
+        Accounts.createUser({
+            username: entry,
+            password: 'test'
+        });
     });
 }
 
-	var user1 = Meteor.users.findOne({ username: 'tester1' });
-	var user2 = Meteor.users.findOne({ username: 'tester2' });
+var users = _.map(usernames, function (username) {
+    return Meteor.users.findOne({username: username});
+});
+
+
+var teamNames = ["Alpha", "Bravo", "Charlie", "Delta"];
 
 if (Teams.find().count() === 0) {
-	Teams.insert({
-		teamName: 'Team 1',
-		teamTag: 'T1',
-		userId: user1._id,
-		author: user1.username,
-	 	submitted: new Date(),
-	 	locked: false,
-	 	members: [{
-      memberId: user1._id,
-      memberRole: 'captain'
-    }]
-	}),
-	Teams.insert({
-		teamName: 'Team 2',
-		teamTag: 'T2',
-		userId: user1._id,
-		author: user1.username,
-	 	submitted: new Date(),
-	 	locked: false,
-	 	members: [{
-      memberId: user1._id,
-      memberRole: 'captain'
-    }]
-	}),
-	Teams.insert({
-		teamName: 'Team 3',
-		teamTag: 'T3',
-		userId: user1._id,
-		author: user1.username,
-	 	submitted: new Date(),
-	 	locked: false,
-	 	members: [{
-      memberId: user1._id,
-      memberRole: 'captain'
-    }]
-	}),
-	Teams.insert({
-		teamName: 'Team 4',
-		teamTag: 'T4',
-		userId: user1._id,
-		author: user1.username,
-	 	submitted: new Date(),
-	 	locked: false,
-	 	members: [{
-      memberId: user1._id,
-      memberRole: 'captain'
-    }]
-	}),
-	Teams.insert({
-		teamName: 'Team 5',
-		teamTag: 'T5',
-		userId: user2._id,
-		author: user2.username,
-	 	submitted: new Date(),
-	 	locked: false,
-	 	members: [{
-      memberId: user2._id,
-      memberRole: 'captain'
-    }]
-	}),
-	Teams.insert({
-		teamName: 'Team 6',
-		teamTag: 'T6',
-		userId: user2._id,
-		author: user2.username,
-	 	submitted: new Date(),
-	 	locked: false,
-	 	members: [{
-      memberId: user2._id,
-      memberRole: 'captain'
-    }]
-	}),
-	Teams.insert({
-		teamName: 'Team 7',
-		teamTag: 'T7',
-		userId: user2._id,
-		author: user2.username,
-	 	submitted: new Date(),
-	 	locked: false,
-	 	members: [{
-      memberId: user2._id,
-      memberRole: 'captain'
-    }]
-	}),
-	Teams.insert({
-		teamName: 'Team 8',
-		teamTag: 'T8',
-		userId: user2._id,
-		author: user2.username,
-	 	submitted: new Date(),
-	 	locked: false,
-	 	members: [{
-      memberId: user2._id,
-      memberRole: 'captain'
-    }]
-	});
+    for (var i = 0; i < teamNames.length; i++) {
+        Teams.insert({
+            teamName: teamNames[i],
+            teamTag: 'T' + i.toString(),
+            userId: users[i]._id,
+            author: users[i].username,
+            submitted: new Date(),
+            locked: false,
+            members: [{memberId: users[i]._id,
+                    memberRole: 'captain'}]
+        });
+    }
 }
+
+var teams = _.map(teamNames, function (teamName) {
+    return {teamId: Teams.findOne({teamName: teamName})._id,
+            seed: 0,
+            points: 0};
+});
 
 if (Tournaments.find().count() === 0) {
 	Tournaments.insert({
-		tournamentName: 'Tournament 1',
-		userId: user1._id,
-		author: user1.username,
+		tournamentName: 'Tournament Awesome',
+        game: "Awesome Game",
+        mode: "Awesome Mode",
+		userId: users[0]._id,
+		author: users[0].username,
 	 	submitted: new Date(),
 	 	locked: false,
-	 	teams: []
+	 	teams: teams
 	});
 }
 
