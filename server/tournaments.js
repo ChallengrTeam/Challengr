d17699b63140803dd765776c4a603e6ba56eaef0
Jackie H <jackiehuynh93@gmail.com>
2015-02-16 15:02:16 +1100

    Moved tournament server methods

---
+++ server/tournaments.js
@@ -0,0 +1,55 @@
+Meteor.methods({
+    tournamentCreateBracket: function(tournamentId, format) {
+        if (format == "None") throw {reason: "Please select a format!"};
+
+        var tournament = Tournaments.findOne({_id: tournamentId});
+        var tournamentTeams = tournament.teams;
+        var numTeams = tournamentTeams.length;
+
+        // min size
+        if (numTeams < 2) throw {reason: "Not enough teams!"};
+        // power of 2   TODO: insert BYEs to fill
+        var logLen = Math.log(numTeams) / Math.LN2;
+        if (parseInt(logLen) !== logLen)
+            throw {reason: "Need number of teams to be a power of 2!"};
+
+        // TODO for seeding, just sort tournamentTeams - already takes first and last for matches
+
+        var roundMatches = numTeams / 2;
+        var rounds = [];
+        while (roundMatches >= 1) {
+            var matches = [];
+            for (var i = 0; i < roundMatches; i++) {
+                if (tournamentTeams.length > 1) {
+                    var team0 = Teams.findOne({_id: (tournamentTeams.shift()).teamId});
+                    var team1 = Teams.findOne({_id: (tournamentTeams.pop()).teamId});
+                    matches.push({
+                        team0: team0._id,
+                        team1: team1._id,
+                        result: null
+                    });
+                } else {
+                    matches.push({
+                        team0: null,
+                        team1: null,
+                        result: null
+                    });
+                }
+            }
+            rounds.push({matches: matches});
+            roundMatches = roundMatches / 2;
+        }
+
+        Tournaments.update(tournamentId, {
+            $set: {format: format,
+                   rounds: rounds}
+        });
+        return {_id: tournamentId};
+    },
+
+    tournamentClearBracket: function(tournamentId) {
+        var tournament = Tournaments.findOne({_id: tournamentId});
+        Tournaments.update(tournamentId, {$unset: {rounds: null}});
+        return {_id: tournamentId};
+    }
+});
