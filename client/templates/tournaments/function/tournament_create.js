e7bb9e8ff54a2aa4f32a7bd2563411f8907c690a
Jackie H <jackiehuynh93@gmail.com>
2015-02-18 17:39:44 +1100

    Added datetimepicker stuff to tournament create page

--- client/templates/tournaments/function/tournament_create.js
+++ client/templates/tournaments/function/tournament_create.js
@@ -1,13 +1,19 @@
+Template.tournamentCreate.rendered = function() {
+    $('.datetimepicker').datetimepicker();
+};
+
 Template.tournamentCreate.events({
   'submit form': function(e) {
     e.preventDefault();
     var post = {
         tournamentName: $(e.target).find('[name=tournamentName]').val(),
-        //this line is wrong, how do I get the date?
-        // tournamentDate: $(e.target).find('[name=tournamentDate]').val(),
         game: $(e.target).find('[name=game]').val(),
         mode: $(e.target).find('[name=mode]').val()
     };
+
+    var startDate = $('.datetimepicker').data('DateTimePicker').date().toString();
+    if (startDate) post.startDate = startDate;
+
     Meteor.call('tournamentInsert', post, function(error, result) {
         // display the error to the user and abort
         if (error)
