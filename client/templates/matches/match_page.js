e08256f39d61dc06ecedcd44b12e201a44201a9d
Jackie H <jackiehuynh93@gmail.com>
2015-02-23 15:23:46 +1100

    Changed tournamentTeamItem to handle null team id

--- client/templates/matches/match_page.js
+++ client/templates/matches/match_page.js
@@ -1,5 +1,5 @@
 Template.matchPage.helpers({
-    getGame: function() {
-        return Tournaments.findOne({_id: this.tournamentId}).game;
+    getName: function() {
+        return Tournaments.findOne({_id: this.tournamentId}).tournamentName;
     }
 });
