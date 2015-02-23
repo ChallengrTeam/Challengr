783984958c4d6d16b11f8b7a4610c2fb9e2684ed
Jackie H <jackiehuynh93@gmail.com>
2015-02-23 15:20:55 +1100

    Added match pages, linked to from tournament bracket page

--- client/templates/tournaments/view/tournament_page_item.js
+++ client/templates/tournaments/view/tournament_page_item.js
@@ -1,53 +1,23 @@
 Template.tournamentPageItem.helpers({
-  ownPost: function() {
- 	  return this.userId === Meteor.userId();
-  },
-  tournamentTeams: function() {
-  	return this.teams;
-  },
-  isLocked: function() {
-  	return this.locked;
-  },
-  formattedDate: function(date) {
-    return date.toLocaleString();
-  }
+    ownPost: function() {
+ 	      return this.userId === Meteor.userId();
+    },
+    tournamentTeams: function() {
+      	return this.teams;
+    },
+    isLocked: function() {
+      	return this.locked;
+    },
+    formattedDate: function(date) {
+        return date.toLocaleString();
+    }
 });
 
 Template.tournamentBracket.helpers({
-  bracketRounds: function() {
-    return this.rounds || [];
-  },
-  getTeamName: function (teamId, defaultString) {
-    var team = Teams.findOne({_id: teamId});
-    return team ? team.teamName : defaultString;
-  }
+    bracketRounds: function() {
+        return this.rounds || [];
+    },
+    getMatch: function (matchId) {
+        return Matches.findOne({_id: matchId});
+    }
 });
-
-// Template.tournamentPageItem.events({
-// 	'click .join': function(e) {
-// 		e.preventDefault();
-// 		var currentTournamentId = this._id;
-// 		console.log(currentTournamentId);
-// 		Meteor.call('tournamentJoin', currentTournamentId, function(error, result) {
-// 	  	// display the error to the user and abort
-// 	  if (error)
-// 	  	return alert(error.reason);
-
-// 	  // Todo: check if member exists just incase
-		
-// 	  });
-// 	},
-// 	'click .leave': function(e) {
-// 		e.preventDefault();
-// 		var currentTournamentId = this._id;
-// 		console.log(currentTournamentId);
-// 		Meteor.call('tournamentLeave', currentTournamentId, function(error, result) {
-// 	  	// display the error to the user and abort
-// 	  if (error)
-// 	  	return alert(error.reason);
-
-// 	  // Todo: check if member exists just incase
-		
-// 	  });
-// 	}
-// });
